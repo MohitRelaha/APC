28ec714ef181ad14929baee5993149ab727eef18
MohitRelaha <43386001+MohitRelaha@users.noreply.github.com>
2019-08-31 00:32:16 +0530

    Rabin-Karp Implementation

--- Strings/rabin_karp.c
+++ Strings/rabin_karp.c
@@ -5,7 +5,6 @@
 int pow(int x, int n)
 {
 	int val = 1;
-	
 	while(n--)
 	{
 		val = val * x;
@@ -15,17 +14,17 @@ int pow(int x, int n)
 }
 
 
-
 void search(char* str, char* ptrn)
 {
-	int h_ptrn = 0 ;
-	int h_str = 0 ;
+	int h_ptrn = 0 ;  //Hash Value of Pattern
+	int h_str = 0 ;   //Hash Value of String
 	int len_str = strlen(str);
 	int len_ptrn = strlen(ptrn);
 	int mod = 1000000000 + 9 ;
-	int prime = 7 ;
+	int prime = 7 ;		// Take any prime no.
 	
 	int i;
+	// Calculating Hash Value for Pattern And for String(for len_ptrn chars)
 	for(i=0; i < len_ptrn ; i++)
 	{
 		h_ptrn += ( (ptrn[i] - 'a' + 1) * pow(prime,i) % mod ) % mod ;
@@ -49,18 +48,11 @@ void search(char* str, char* ptrn)
 				
 		}
 	
-	
 	for(i=1; i <= len_str - len_ptrn; i++)
-	{
-		
-		
-		if(i <= len_str - len_ptrn)
-		{
-			int old_hash = h_str ;   // extra variable for sake of better understanding
-			int new_hash = ( ((old_hash - (str[i-1] - 'a')) / prime)  + ( ((str[i+len_ptrn-1] - 'a' + 1) * (pow(prime,len_ptrn-1) % mod)) % mod ) ) % mod;
-			h_str = new_hash ;
-			
-		}
+	{	
+		int old_hash = h_str ;   // extra variable for sake of better understanding
+		int new_hash = ( ((old_hash - (str[i-1] - 'a')) / prime)  + ( ((str[i+len_ptrn-1] - 'a' + 1) * (pow(prime,len_ptrn-1) % mod)) % mod ) ) % mod;
+		h_str = new_hash ;
 		
 	//	printf("%d  %d\n",h_ptrn,h_str);
 	
@@ -77,8 +69,7 @@ void search(char* str, char* ptrn)
 				printf("Pattern found a Index : %d\n",i);
 				
 		}
-	
-	
+		
 	}
 	
 }
